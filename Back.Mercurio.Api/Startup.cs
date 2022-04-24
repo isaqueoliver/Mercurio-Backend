1c8e0581b84b1bf6ecb12acdd8441cae6f2ae441
Isaque Oliveira <isaque_dornelas@hotmail.com>
2022-04-24 17:23:42 -0300

    começo da parte de backend para o tcc

---
+++ Back.Mercurio.Api/Startup.cs
@@ -0,0 +1,34 @@
+﻿namespace Back.Mercurio.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; set; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment environment)
+        {
+            if(app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+        }
+    }
+}
