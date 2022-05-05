2681510e6009984be1ca4fe2c5d6377a0c6b1822
Isaque Oliveira <isaque_dornelas@hotmail.com>
2022-05-05 20:18:36 -0300

    nova conta e autenticacao funcionando perfeitamente, ja feita tbem a primeira migration para formação do banco

---
+++ Back.Mercurio.Infrastructure/IRepository/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using Back.Mercurio.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Back.Mercurio.Infrastructure.IRepository
+{
+    public interface IRepository<T> : IDisposable where T : IAggregateRoot
+    {
+        IUnitOfWork UnitOfWork { get; }
+    }
+}
